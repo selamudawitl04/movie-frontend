9d70b0fc0f74787007086b9fda9f7fd5c73f7530
Selamu Dawit <88835502+selamudawitl04@users.noreply.github.com>
2023-05-27 09:39:51 +0300

    Delete apollo directory

--- apollo/client.js
+++
@@ -1,8 +0,0 @@
-import { defineApolloClient } from "@nuxtjs/apollo";
-export default defineApolloClient({
-  httpEndpoint: "https://vue-shopping.hasura.app/v1/graphql",
-  tokenStorage: "cookie",
-  authType: "Bearer",
-  authHeader: "Authorization",
-  tokenName: 'auth_token',
-});
