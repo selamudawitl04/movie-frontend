7a7cef5f98c15c77c26c60f74190d0e59a3a8dba
Selamu Dawit <88835502+selamudawitl04@users.noreply.github.com>
2023-05-27 09:41:01 +0300

    Delete components directory

--- components/NewCounter.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <h1>{{ counterStore.getCounter }}</h1>
-    <button @click="counterStore.increment">Add Counter</button>
-</template>
-<script setup>
-
-const counterStore = useCounterStore();
-</script>
